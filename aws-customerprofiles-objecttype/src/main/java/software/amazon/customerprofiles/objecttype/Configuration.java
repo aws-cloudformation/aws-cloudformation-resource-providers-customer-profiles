ea7a251cacb74ded7bd8e250e485c17699e49d26
lanxin-zhang-work <75397705+lanxin-zhang-work@users.noreply.github.com>
2022-07-27 11:14:54 -0700

    Fix resource level tags handling (#30)
    * Update on resource level tag support

--- aws-customerprofiles-objecttype/src/main/java/software/amazon/customerprofiles/objecttype/Configuration.java
+++ aws-customerprofiles-objecttype/src/main/java/software/amazon/customerprofiles/objecttype/Configuration.java
@@ -1,8 +1,24 @@
 package software.amazon.customerprofiles.objecttype;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 class Configuration extends BaseConfiguration {
 
     public Configuration() {
         super("aws-customerprofiles-objecttype.json");
+    }
+
+    /**
+     * Providers should implement this method if their resource has a 'Tags' property to define resource-level tags
+     * @return tags
+     */
+    @Override
+    public Map<String, String> resourceDefinedTags(final ResourceModel resourceModel) {
+        if (resourceModel.getTags() == null) {
+            return null;
+        } else {
+            return resourceModel.getTags().stream().collect(Collectors.toMap(Tag::getKey, Tag::getValue, (value1, value2) -> value2));
+        }
     }
 }
